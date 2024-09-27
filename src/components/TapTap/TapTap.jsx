769418e9f8a1efb63426b40533de637cc85e0b3c
Vladislav <vladislavsafarov04@mail.ru>
2024-09-27 20:29:42 +0300

    Add random words

--- src/components/TapTap/TapTap.jsx
+++ src/components/TapTap/TapTap.jsx
@@ -1,13 +1,12 @@
 import { useState } from "react";
 import "./tapTap.css"
 import { useSelector } from "react-redux";
+import { generate, count } from "random-words";
 
-const allText = ["Вояж ", "Глубокомыслие ", "Гряда ", "Застрелить ", "Знамение ", "Каркать ", "Опрос ", "Подсудный ", "Ракурс ", "Самогонщик ",
-    "Амортизатор ", "Аппендикс ", "Вверзиться ", "Гречневый ", "Долголетие ", "Отбой ", "Похвала ",
-]
+const randomWords = generate(10);
 
 function TapTap() {
-    const [text, setText] = useState(allText)
+    const [text, setText] = useState(randomWords)
     const theme = useSelector((state) => state.theme.theme);
 
     return (
@@ -19,7 +18,9 @@ function TapTap() {
                 <h1>Fast Tap Tap Fingers</h1>
                 <p>A simple game where you tap your fingers quickly and accurately to score points.</p>
                 <div className="container">
-                    {text}
+                    {text.map((word, index) => {
+                        return index === 10 ? <br /> : <span className="word">{word} </span> 
+                    })}
                 </div>
             </div>
             <div>
